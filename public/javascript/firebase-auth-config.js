1097a2d03c476310f3bd83c1ba478858315cbacc
Harrison Milbradt <harrison.milbradt@gmail.com>
2017-05-02 16:58:55 -0700

    Revised landing page.  Removed unnecessary files and html.  Revised style.  IN PROGRESS: moving scripts to seperate files

--- public/javascript/firebase-auth-config.js
+++ public/javascript/firebase-auth-config.js
@@ -1,6 +1,6 @@
 // FirebaseUI config.
 var uiConfig = {
-signInSuccessUrl: '<url-to-redirect-to-on-success>',
+signInSuccessUrl: 'dashboard.html',
 signInOptions: [
   // Leave the lines as is for the providers you want to offer your users.
   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
