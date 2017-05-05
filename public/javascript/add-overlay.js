4faffa89523ae13e654a1ec14856867455de22c1
Rost Myshkin <rdavidoff@hotmail.ca>
2017-05-04 20:39:53 -0700

    added a back button in the overlay and added 12 fruit buttons with animations

--- public/javascript/add-overlay.js
+++ public/javascript/add-overlay.js
@@ -2,7 +2,6 @@
 /* Open when someone clicks on the span element */
 function openNav() {
     document.getElementById("myNav").style.height = "100%";
-    
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
